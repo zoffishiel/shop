052f0243cdc75c6ce53c3d667f1ec97a7145c19a
zakariae21 <zakariae.zeddouk@outlook.com>
2019-01-06 23:46:46 +0100

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,46 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(["prefix" => '/dashboard', 'middleware' => 'auth'], function(){
+  Route::get('/', function(){
+    return view('dashboard.index');
+  })->name('dashboard.index');
+
+  Route::get('/clients', function(){
+    return view('dashboard.clients');
+  })->name('dashboard.clients');
+
+  Route::get('/collections', function(){
+    return view('dashboard.collections');
+  })->name('dashboard.collections');
+
+  Route::get('/commandes', function(){
+    return view('dashboard.commandes');
+  })->name('dashboard.commandes');
+
+  Route::get('/produits', function(){
+    return view('dashboard.produits');
+  })->name('dashboard.produits');
+
+  Route::get('/categories', function(){
+    return view('dashboard.categories');
+  })->name('dashboard.categories');
+
+  Route::get('/messages', function(){
+    return view('dashboard.messages');
+  })->name('dashboard.messages');
+
+  Route::get('/utilisateurs', function(){
+    return view('dashboard.utilisateurs');
+  })->name('dashboard.utilisateurs');
+
+  Route::get('/parametres', function(){
+    return view('dashboard.parametres');
+  })->name('dashboard.parametres');
+});
+
+Route::get('/logout', function(){
+  Auth::logout();
+  return Redirect::to('/login');
+});
