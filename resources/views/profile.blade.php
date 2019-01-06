3a4ace1d51dcd84da691bd32b43e45e75c655d06
mahdi <mkelectro98@gmail.com>
2019-01-06 22:55:28 +0100

    Added Profile View

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,244 @@
+@extends('layouts.app')
+@section('content')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    {{-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css"> --}}
+    <link rel="stylesheet" href="{{ mix('/css/app.css')}}">
+
+</head>
+
+<body>
+    <div id="Data">
+        <div class="container emp-profile">
+            <form method="post">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="profile-img">
+                            <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?dl&fit=crop&crop=entropy&w=640&h=546" alt="" />
+
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="profile-head">
+                            <h5 id="name">
+                                dianne pierce
+                            </h5>
+
+                            <h6>
+                                Web Developer and Designer
+                            </h6>
+                            <p class="proile-rating">RANKINGS :
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star"></span>
+                            </p>
+                            <div class="col-md-12" style="display:inline">
+
+
+                                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Timeline</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Edit Profile" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="profile-work">
+                            <p>WORK LINK</p>
+                            <hr size="30">
+                            <a href=""><i class="fas fa-link"></i>Website Link</a><br />
+                            <a href="" data-toggle="modal" data-target="#Facebook"><i class="fab fa-facebook-square"></i>Facebook</a><br />
+                            <a href="" data-toggle="modal" data-target="#whatsapp"><i class="fab fa-whatsapp"></i>Whatsapp</a><br />
+                            <a href="" data-toggle="modal" data-target="#instagram"><i class="fab fa-instagram"></i>Instagram</a>
+                            <p>SKILLS</p>
+                            <hr size="30">
+
+                            <a href="">Communication</a><br />
+                            <a href="">Collaboration</a><br />
+                            <a href="">E-commerce</a><br />
+                            <a href="">Designer</a><br />
+                            <a href="">Programmer</a><br />
+                        </div>
+                    </div>
+                    <div class="modal fade" tabindex="-1" role="dialog" id="Facebook">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title"><i class="fab fa-facebook-square"></i>Facebook :</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <label for="FbLink">Link :</label>
+                                    <input type="text" class="form-control" id="FbLink" placeholder="https://www.facebook.com/.....">
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary" data-dismiss="modal">Save</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="modal fade" tabindex="-1" role="dialog" id="whatsapp">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title"><i class="fab fa-whatsapp"></i>Whatsapp :</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <label for="whatsapplink">Link :</label>
+                                    <input type="text" class="form-control" id="whatsapplink" >
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary" data-dismiss="modal">Save</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="modal fade" tabindex="-1" role="dialog" id="instagram">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title"><i class="fab fa-instagram"></i>Instagram :</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <label for="instagramlink">Link :</label>
+                                    <input type="text" class="form-control" id="instagram" >
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary" data-dismiss="modal">Save</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="tab-content profile-tab" id="myTabContent">
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <label>User Id</label>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <p>dianne12</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <label>Name</label>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <p>dianne pierce</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <label>Email</label>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <p>dianne.pierce69
+                                            @example.com</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <label>Phone</label>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <p>(886)-234-1498</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <label>Profession</label>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <p>Web Developer and Designer</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <label>Experience</label>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <p>Expert</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <label>Hourly Rate</label>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <p>10$/hr</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <label>Total Projects</label>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <p>230</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <label>English Level</label>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <p>Expert</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <label>Availability</label>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <p>6 months</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <label>Your Bio</label><br />
+                                        <p>Your detail description</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
+@endsection
