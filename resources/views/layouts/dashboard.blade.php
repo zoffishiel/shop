35056613913add07817180c7f4430b9d07eb6299
zakariae21 <zakariae.zeddouk@outlook.com>
2019-01-08 01:20:40 +0100

    Update design

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -25,6 +25,7 @@
     <header class="navbar navbar-expand-lg navbar-light border-bottom">
       <div class="container">
         <a href="{{ route('dashboard.index') }}" class="navbar-brand">LOGO</a>
+        <button type="button" id="sidebar-toggle" class="btn btn-outline-primary ml-5" name="button"><i class="fa fa-bars"></i> </button>
         <ul class="navbar-nav ml-auto">
           <li class="nav-item mr-5 mt-2">
             <i class="fa fa-bell"></i>
@@ -42,39 +43,39 @@
 
       <div class="row container-fluid" id="body">
         <div class="col-md-2" id="sidebar">
-          <h5 class="text-center mt-4">STORE</h5>
+          <h5 class="text-center mt-4 sidebar-text">STORE</h5>
           <div class="list-group pt-3">
             <a href="{{ route('dashboard.index') }}" class="list-group-item">
               <i class="fa fa-dashboard mr-3"></i>
-              Dashboard
+              <i class="sidebar-text">Dashboard</i>
             </a>
             <a href="{{ route('dashboard.commandes') }}" class="list-group-item">
               <i class="fa fa-shopping-cart mr-3"></i>
-              Commandes
+              <i class="sidebar-text">Commandes</i>
             </a>
             <a href="{{ route('dashboard.categories') }}" class="list-group-item">
               <i class="fa fa-archive mr-3"></i>
-              Catégories
+              <i class="sidebar-text">Catégories</i>
             </a>
             <a href="{{ route('dashboard.produits') }}" class="list-group-item">
               <i class="fa fa-tag mr-3"></i>
-              Produits
+              <i class="sidebar-text">Produits</i>
             </a>
             <a href="{{ route('dashboard.utilisateurs') }}" class="list-group-item">
               <i class="fa fa-users mr-3"></i>
-              Utilisateurs
+              <i class="sidebar-text">Utilisateurs</i>
             </a>
             <a href="{{ route('dashboard.clients') }}" class="list-group-item">
               <i class="fa fa-user mr-3"></i>
-              Clients
+              <i class="sidebar-text">Clients</i>
             </a>
             <a href="{{ route('dashboard.messages') }}" class="list-group-item">
               <i class="fa fa-envelope mr-3"></i>
-              Messages
+              <i class="sidebar-text">Messages</i>
             </a>
             <a href="{{ route('dashboard.parametres') }}" class="list-group-item">
               <i class="fa fa-cog mr-3"></i>
-              Paramètres
+              <i class="sidebar-text">Paramètres</i>
             </a>
 
           </div>
