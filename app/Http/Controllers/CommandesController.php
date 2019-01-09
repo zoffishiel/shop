1b4703462bc3bb41789e137377212dc7fb3526fa
zakariae21 <zakariae.zeddouk@outlook.com>
2019-01-09 09:03:53 +0100

    Modifier addCommande

--- app/Http/Controllers/CommandesController.php
+++ app/Http/Controllers/CommandesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Commandes;
 
 class CommandesController extends Controller
@@ -15,9 +16,30 @@ class CommandesController extends Controller
 
     public function addCommande(Request $request)
     {
-      $string = strtoupper(str_random(3)) . random_int(1000000,9999999);
-      // return $string;
-      return response()->json($string, 200);
+      $rules = [
+        'serie' => ['bail', 'required', 'string', 'max:10', 'unique:commandes'],
+        'produit' => ['required'],
+        'vendeur' => ['required'],
+        'date' => ['date'],
+        'prix' => 'required',
+        'qte' => 'required',
+        'service' => 'string',
+        'adresse' => ['required', 'string', 'max:255'],
+        'ville' => ['required', 'string', 'max:100'],
+        'tel' => ['required', 'string', 'max:13'],
+        'nom_client' => ['required', 'string', 'max:100'],
+        'commantaire' => 'string',
+      ];
+
+      $string = strtoupper(str_random(10));
+      $request['serie'] = $string;
+      $validator = Validator::make($request->all(), $rules);
+      if($validator->fails()){
+        return response()->json($validator->errors(), 200);
+      }else{
+        $commande = Commandes::create($request->all());
+        return $commande ? 1 : 0;
+      }
     }
 
     public function dropCommande($id)
@@ -27,6 +49,18 @@ class CommandesController extends Controller
         return 0;
       }else{
         $commande->delete();
+        return 1;
+      }
+    }
+
+    public function updateStatus($id, $status)
+    {
+      $commande = Commandes::find($id);
+      if(is_null($commande)){
+        return 0;
+      }else{
+        $commande->statut = $status;
+        $commande->save();
         return 1;
       }
     }
