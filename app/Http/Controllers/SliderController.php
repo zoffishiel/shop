e0e40947d5bef99dc3db30768a58a76f51d50dd4
zakariae21 <zakariae.zeddouk@outlook.com>
2019-01-09 20:27:23 +0100

    Add functions

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Slider;
 
 class SliderController extends Controller
 {
-    //
+    public function index()
+    {
+      $sliders = Slider::All();
+      return response()->json($sliders, 200);
+    }
+
+    public function addSlider()
+    {
+
+    }
+
+    public function updateSlider($id)
+    {
+
+    }
+
+    public function dropSlider($id)
+    {
+      $slider = Slider::find($id)->delete();
+      return $slider ? 1 : 0;
+    }
 }
