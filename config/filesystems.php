5f55c4cd83cd1adfdd356c333ed628636c176b67
zakariae21 <zakariae.zeddouk@outlook.com>
2019-01-11 16:11:39 +0100

    Add image storage path

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,6 +54,12 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
+        // ADD IMAGES STORAGE
+        'images' => [
+          'driver' => 'local',
+          'root' => public_path(),
+          'visibility' => 'public',
+        ],
 
         's3' => [
             'driver' => 's3',
