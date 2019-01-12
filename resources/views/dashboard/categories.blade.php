44e9c819ff34de985f87f2ab3d373e0b9f9896a7
zakariae21 <zakariae.zeddouk@outlook.com>
2019-01-12 13:53:28 +0100

    Modifier modal & table

--- resources/views/dashboard/categories.blade.php
+++ resources/views/dashboard/categories.blade.php
@@ -15,31 +15,15 @@
 
     <table id="categories" class="hover striped border ">
         <thead>
-            <th>Id</th>
-            <th>Nom</th>
-            <th>Nbr Produits</th>
-            <th>Action</th>
+            
         </thead>
-        <tbody>
-          <tr>
-          <td>1</td>
-          <td>Phones</td>
-          <td>20</td>
-          <td>
-            <div class="btn-group row ml-2">
-                <button type="button" class="btn btn-danger col-md-6" name="button"><i class="fa fa-trash"></i></button>
-                <button type="button" class="btn btn-primary col-md-6" name="button" ><i class="fas fa-edit"></i></button>
-            </div>
-          </td>
-            </tr>
-        </tbody>
     </table>
 </div>
 <div class="modal fade" tabindex="-1" role="dialog" id="add">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title"><i class="fas fa-plus mr-3"></i>Ajouter une catégorie :</h5>
+                <h5 class="modal-title">Ajouter un catégorie :</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -48,22 +32,13 @@
                 <div class="col-md-12">
                     <div class=" mt-3">
                         <div class="form-group">
-                            <label for="Nom" class=" ml-3 ">Nom :</label>
-                            <input type="text" class="form-control " id="Nom"  >
+                            <label for="Nom" class="ml-3">Nom :</label>
+                            <input type="text" name="nom" class="form-control" id="Nom">
                         </div>
-                    </div>
-                    <div class="mt-3">
-                        <div class="form-group">
-                            <label for="nbrprod" class=" ml-3  ">Nbr Produits : </label>
-                            <input type="number" class="form-control " id="nbrprod" >
-                        </div>
-                    </div>
-
-
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Save</button>
+                <button type="button" id="ajouterBtn" class="btn btn-primary" data-dismiss="modal">Ajouter</button>
             </div>
         </div>
     </div>
@@ -75,11 +50,41 @@
 <script type="text/javascript">
     $(function() {
         $("#categories").bootstrapTable({
+            columns : [
+              {
+                checkbox : true,
+                align : 'center',
+              },{
+                field : 'id',
+                title :  'ID',
+                align : 'center',
+                sortable : true,
+              },{
+                field : 'nom',
+                title : 'Nom',
+                align : 'center',
+                sortable : true,
+              },{
+                field : 'nbrProduits',
+                title : 'Nbr Produits',
+                align : 'center',
+              }
+            ],
             url: '/api/categories',
             showRefresh: true,
             search: true,
             sorting: true,
             toolbar: '#toolbar',
+        });
+        $("#ajouterBtn").on('click', function(){
+          var data = {"nom" : $("#Nom").val()};
+          $.post('/api/add/category', data, (resp) =>{
+            if(resp){
+              $("#categories").bootstrapTable("refresh");
+            }else{
+              console.log("error");
+            }
+          });
         });
     });
 </script>
