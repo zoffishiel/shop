2ff48b596df75fa7124f8ad496d70522614f7733
zakariae21 <zakariae.zeddouk@outlook.com>
2019-01-12 16:28:27 +0100

    Page d'ajout du commande

---
+++ resources/views/dashboard/ajouter_commande.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.dashboard')
+
+@section('content')
+  <div class="container card">
+    <h4 class="text-center mt-3 mb-5">Ajouter Commande</h4>
+    <div class="card-content">
+      <form class="" action="#" method="post">
+        <div class="form-inline mb-3">
+          <label class="col-md-2" for="">Nom Client : </label>
+          <input class="form-control col-md-4" type="text" name="nom" value="">
+        </div>
+        <div class="form-inline mb-3">
+          <label class="col-md-2" for="">Telephone : </label>
+          <input class="form-control col-md-4" type="text" name="nom" value="">
+        </div>
+        <div class="form-inline mb-3">
+          <label class="col-md-2" for="">Ville : </label>
+          <input class="form-control col-md-4" type="text" name="nom" value="">
+        </div>
+        <div class="form-inline mb-3">
+          <label class="col-md-2" for="">Adresse : </label>
+          <input class="form-control col-md-4" type="text" name="nom" value="">
+        </div>
+        <div class="form-inline mb-3">
+          <label class="col-md-2" for="">Service Livraison : </label>
+          <input class="form-control col-md-4" type="text" name="nom" value="">
+        </div>
+      </form>
+    </div>
+  </div>
+@endsection
