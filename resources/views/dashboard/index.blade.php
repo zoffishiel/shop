65ae1efc7f797fec441f14aba514bae084d1da23
zakariae21 <zakariae.zeddouk@outlook.com>
2019-01-13 18:05:29 +0100

    Update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -10,25 +10,25 @@
 @endsection
 
 @section('content')
-  <div class="row mx-auto ">
-    <div class="col-md-12 ">
+  <div class="row mx-auto">
+    <div class="col-md-11 offset-1">
       <div class="row">
-        <div class="col-md-3  card">
+        <div class="col-md-3 card">
           <div class="card-body text-center">
             <h5 class="card-title">Solde Garentie</h5>
-            <p>0 DH</p>
+            <p>{{ Auth::user()->garantie }} DH</p>
           </div>
         </div>
         <div class="col-md-3 offset-md-1 card">
           <div class="card-body text-center">
             <h5 class="card-title">Solde Actuel</h5>
-            <p>0 DH</p>
+            <p>{{ Auth::user()->actuel }} DH</p>
           </div>
         </div>
         <div class="col-md-3 offset-md-1 card">
           <div class="card-body text-center">
             <h5 class="card-title">Solde Total</h5>
-            <p>0 DH</p>
+            <p>{{ Auth::user()->total }} DH</p>
           </div>
         </div>
       </div>
@@ -41,13 +41,13 @@
         <div class="card">
           <h5 class="text-center mt-3">Revenus</h5>
           <div class="card-content">
-            <canvas id="revenus-canvas"  ></canvas>
+            <canvas id="revenus-canvas"></canvas>
           </div>
         </div>
       </div>
       <div class="col-md-6">
         <div class="card">
-          <h5 class="text-center mt-3">Ventes et Achats</h5>
+          <h5 class="text-center mt-3">Ventes</h5>
           <div class="card-content">
             <canvas id="Ventes-canvas"></canvas>
           </div>
@@ -58,10 +58,13 @@
 
   </div>
 
-<div class="mt-3">
+<div class="mt-3 mb-5">
   <div class="card text-center">
     <h4 class="card-title mt-3">Commandes</h4>
     <div class="card-body">
+      <div id="toolbar" class="btn-group">
+        <a href="{{ route('dashboard.add_commande') }}" id="add" role="button" class="btn btn-success"><i class="fa fa-plus"></i> Ajouter</a>
+      </div>
       <table id="commandes" class="border hover striped">
         <thead>
           <th>Track</th>
@@ -80,20 +83,25 @@
 @endsection
 
 @section('js')
-  <script src="{{ asset('js/bootstrap-table/bootstrap-table.min.js') }}" charset="utf-8"></script>
+  <script src="{{ asset('js/bootstrap-table/bootstrap-table.js') }}" charset="utf-8"></script>
   <script src="{{ asset('js/Chart.bundle.min.js') }}" charset="utf-8"></script>
   <script type="text/javascript">
     $(function(){
+      var mois = ["Jan", "Fev", "Mars", "Avr", "Mai", "Juin", "Juil", "Aout", "Sept", "Oct", "Nov", "Dec"];
       $("#commandes").bootstrapTable({
         url : '/api/commandes',
+        toolbar : '#toolbar',
         search: true,
-        refresh: true,
+        showRefresh: true,
+        pagination : true,
+        pageSize : 5,
+        pageList : [5, 10, 25, 50, "ALL"]
       });
       var ctx = document.getElementById("revenus-canvas").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: mois,
         datasets: [{
             label: '# of Votes',
             data: [12, 4, 3, 5, 2, 3],
@@ -130,9 +138,9 @@ var ctx2 = document.getElementById("Ventes-canvas").getContext('2d');
 var myLineChart = new Chart(ctx2, {
     type: 'line',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: mois,
         datasets: [{
-            label: '# Number of sels',
+            label: '# Number of sells',
             data: [12, 19, 3, 15, 22, 13],
             backgroundColor: [
 
--- resources/views/dashboard/messages.blade.php
+++ resources/views/dashboard/messages.blade.php
@@ -1,5 +1,53 @@
 @extends('layouts.dashboard')
 
+@section('css')
+  <link rel="stylesheet" href="{{ asset('css/bootstrap-table/bootstrap-table.min.css') }}">
+@endsection
+@section('content')
+  <div class="mt-3 p-3 card">
+    <h4 class="my-4 text-center">Messages</h4>
+    <div class="btn-group" id="toolbar">
+      <button type="button" class="btn btn-danger" name="button"><i class="fa fa-trash"></i> </button>
+    </div>
+    <table id="messages" class="table striped hover">
+
+    </table>
+  </div>
+
+@endsection
 @section('js')
-  
+  <script src="{{ asset('js/bootstrap-table/bootstrap-table.js') }}" charset="utf-8"></script>
+  <script type="text/javascript">
+    $(function(){
+      $("#messages").bootstrapTable({
+        columns : [
+          {
+            field : 'sujet',
+            title : 'Sujet',
+            align : 'center',
+          },{
+            field : 'commentaire',
+            title : 'Commentaire',
+            align : 'center',
+          },{
+            field : 'ville',
+            title : 'Ville',
+            align : 'center',
+          },{
+            field : 'adresse',
+            title : 'Adresse',
+            align : 'center',
+          },{
+            field : 'date',
+            title : 'Date',
+            align : 'center',
+          }
+        ],
+        url : '/api/messages',
+        toolbar : '#toolbar',
+        search : true,
+        showRefresh : true,
+      });
+    });
+  </script>
 @endsection
